ba5a9dfc4e33dff81dd86520dd943172172d80e6
kyong-dev <kyong.dev@gmail.com>
2024-12-28 14:08:20 +0900

    custom log admin

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -71,4 +71,4 @@ if settings.DEBUG:
     ]
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ]
--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import Group
-from unfold.admin import ModelAdmin
+from dashboard.admin import ModelAdmin
 from .models import User
 
 
